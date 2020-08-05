090b1967c1e04c4aa62d91fea587319def7fc9fc
DonovanAdrian <websurfer21020@gmail.com>
2020-08-05 13:56:37 -0600

    Updated index Files
    With this commit, I've completely overhauled the index page. Well, to be more accurate, I shuffled some stuff around. It now loads everything in a particular order AND you can now load in a config file of your choice! Just fill in the database info on /txt/config.txt and you'll be all set to go!
    
    Another challenge that I hadn't thought about before was: "What if the user tries to sign up or login before the config is loaded?". Well, I fixed that problem, as the buttons now only work if the config and database have been properly initialized. Hopefully this should solve that problem!

--- src/js/indexAlg.js
+++ src/js/indexAlg.js
@@ -1,6 +1,8 @@
 var listeningFirebaseRefs = [];
 var userArr = [];
 
+var configObj = {};
+
 var loginBool = false;
 var reload = false;
 
@@ -10,37 +12,125 @@ var pin;
 var offlineSpan;
 var offlineModal;
 var validUser;
+var loginBtn;
+var signUpFld;
 
+function fetchConfigFile(){
+  console.log("Fetching Config");
+  var file = "txt/config.txt";
+
+  $.ajax({
+    url: file,
+    success: function(data){
+      var configInitializeInt = 0;
+      var configFileInput = data.split('\n');
+      var isComment;
+      var apiKeyString = "";
+      var authDomainString = "";
+      var databaseURLString = "";
+      var projectIdString = "";
+      var storageBucketString = "";
+      var messagingSenderIdString = "";
+      var appIdString = "";
+      var measurementIdString = "";
+
+      for(var i = 0; i < configFileInput.length; i++){
+        isComment = false;
+        if (configFileInput[i].charAt(0) == "#" || configFileInput[i].charAt(0) == "") {
+          isComment = true;
+        }
+        if (!isComment) {
+          if (configFileInput[i].includes("apiKey:")){
+            configInitializeInt++;
+            apiKeyString = configFileInput[i].substr(7, configFileInput[i].length);
+            apiKeyString = apiKeyString.replace(/"/g, '');
+            apiKeyString = apiKeyString.replace(/,/g, '');
+            apiKeyString = apiKeyString.replace(/ /g, '');
+          } else if (configFileInput[i].includes("authDomain:")){
+            configInitializeInt++;
+            authDomainString = configFileInput[i].substr(11, configFileInput[i].length);
+            authDomainString = authDomainString.replace(/"/g, '');
+            authDomainString = authDomainString.replace(/,/g, '');
+            authDomainString = authDomainString.replace(/ /g, '');
+          } else if (configFileInput[i].includes("databaseURL:")){
+            configInitializeInt++;
+            databaseURLString = configFileInput[i].substr(12, configFileInput[i].length);
+            databaseURLString = databaseURLString.replace(/"/g, '');
+            databaseURLString = databaseURLString.replace(/,/g, '');
+            databaseURLString = databaseURLString.replace(/ /g, '');
+          } else if (configFileInput[i].includes("projectId:")){
+            configInitializeInt++;
+            projectIdString = configFileInput[i].substr(10, configFileInput[i].length);
+            projectIdString = projectIdString.replace(/"/g, '');
+            projectIdString = projectIdString.replace(/,/g, '');
+            projectIdString = projectIdString.replace(/ /g, '');
+          } else if (configFileInput[i].includes("storageBucket:")){
+            configInitializeInt++;
+            storageBucketString = configFileInput[i].substr(14, configFileInput[i].length);
+            storageBucketString = storageBucketString.replace(/"/g, '');
+            storageBucketString = storageBucketString.replace(/,/g, '');
+            storageBucketString = storageBucketString.replace(/ /g, '');
+          } else if (configFileInput[i].includes("messagingSenderId:")){
+            configInitializeInt++;
+            messagingSenderIdString = configFileInput[i].substr(18, configFileInput[i].length);
+            messagingSenderIdString = messagingSenderIdString.replace(/"/g, '');
+            messagingSenderIdString = messagingSenderIdString.replace(/,/g, '');
+            messagingSenderIdString = messagingSenderIdString.replace(/ /g, '');
+          } else if (configFileInput[i].includes("appId:")){
+            configInitializeInt++;
+            appIdString = configFileInput[i].substr(6, configFileInput[i].length);
+            appIdString = appIdString.replace(/"/g, '');
+            appIdString = appIdString.replace(/,/g, '');
+            appIdString = appIdString.replace(/ /g, '');
+          } else if (configFileInput[i].includes("measurementId:")){
+            configInitializeInt++;
+            measurementIdString = configFileInput[i].substr(14, configFileInput[i].length);
+            measurementIdString = measurementIdString.replace(/"/g, '');
+            measurementIdString = measurementIdString.replace(/,/g, '');
+            measurementIdString = measurementIdString.replace(/ /g, '');
+          } else {
+            //console.log("Unknown Config String, Please Advise:");
+            //console.log(configFileInput[i]);
+          }
+        } else {
+          //console.log("Comment Found!");
+        }
+      }
+
+      if(configInitializeInt == 8){
+        if (apiKeyString == "" || authDomainString == "" || databaseURLString == "" || projectIdString == "" ||
+          storageBucketString == "" || messagingSenderIdString == "" || appIdString == "" || measurementIdString == "") {
+          alert("Config not properly initialized! Please contact an administrator!");
+          console.log("Config Not Initialized! Are You Using The Default Config File?");
+        } else {
+          configObj = {
+            apiKey: apiKeyString,
+            authDomain: authDomainString,
+            databaseURL: databaseURLString,
+            projectId: projectIdString,
+            storageBucket: storageBucketString,
+            messagingSenderId: messagingSenderIdString,
+            appId: appIdString,
+            measurementId: measurementIdString
+          };
+          console.log("Config Successfully Initialized!");
+
+          sessionStorage.setItem("config", JSON.stringify(configObj));
+          initializeDatabase();
+        }
+      }
+    }
+  });
+}
 
 window.onload = function instantiate() {
 
   username = document.getElementById("username");
   pin = document.getElementById("pin");
-
-  offlineModal = document.getElementById('offlineModal');
+  offlineModal = document.getElementById("offlineModal");
   offlineSpan = document.getElementById("closeOffline");
-
-  const config = {
-    //Oops! This is gone!
-  };
-  firebase.initializeApp(config);
-  firebase.analytics();
-
-  firebase.auth().signInAnonymously().catch(function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
-
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // User is signed in.
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-    } else {
-      // User is signed out.
-    }
-  });
-
+  loginBtn = document.getElementById("loginBtn");
+  signUpFld = document.getElementById("signUpFld");
 
   window.addEventListener("online", function(){
     offlineModal.style.display = "none";
@@ -70,50 +160,82 @@ window.onload = function instantiate() {
     }
   };
 
-  databaseQuery();
-
-  function databaseQuery() {
-
-    userInitial = firebase.database().ref("users/");
-
-    var fetchPosts = function (postRef) {
-      postRef.on('child_added', function (data) {
-        userArr.push(data.val());
-      });
-
-      postRef.on('child_changed', function (data) {
-        var i = findUIDItemInArr(data.key, userArr);
-        if(userArr[i] != data.val() && i != -1){
-          //console.log("Updating " + userArr[i].userName + " to most updated version: " + data.val().userName);
-          userArr[i] = data;
-        }
-      });
-
-      postRef.on('child_removed', function (data) {
-        var i = findUIDItemInArr(data.key, userArr);
-        if(userArr[i] != data.val() && i != -1){
-          //console.log("Removing " + userArr[i].userName + " / " + data.val().userName);
-          userArr.splice(i, 1);
-        }
-      });
-    };
-
-    fetchPosts(userInitial);
-
-    listeningFirebaseRefs.push(userInitial);
-  }
-
-  function findUIDItemInArr(item, userArray){
-    for(var i = 0; i < userArray.length; i++){
-      if(userArray[i].uid == item){
-        console.log("Found item: " + item);
-        return i;
-      }
-    }
-    return -1;
-  }
+  fetchConfigFile();
 };
 
+function initializeDatabase(){
+
+  console.log("Initializing Database");
+
+  firebase.initializeApp(configObj);
+  firebase.analytics();
+
+  firebase.auth().signInAnonymously().catch(function (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+    } else {
+      // User is signed out.
+    }
+  });
+
+  console.log("Database Successfully Initialized!");
+  databaseQuery();
+  loginBtn.onclick = function(){
+    login();
+  }
+  signUpFld.onclick = function(){
+    signUp();
+  }
+}
+
+function databaseQuery() {
+
+  console.log("Fetching Users From Database");
+  userInitial = firebase.database().ref("users/");
+
+  var fetchPosts = function (postRef) {
+    postRef.on('child_added', function (data) {
+      userArr.push(data.val());
+    });
+
+    postRef.on('child_changed', function (data) {
+      var i = findUIDItemInArr(data.key, userArr);
+      if(userArr[i] != data.val() && i != -1){
+        //console.log("Updating " + userArr[i].userName + " to most updated version: " + data.val().userName);
+        userArr[i] = data;
+      }
+    });
+
+    postRef.on('child_removed', function (data) {
+      var i = findUIDItemInArr(data.key, userArr);
+      if(userArr[i] != data.val() && i != -1){
+        //console.log("Removing " + userArr[i].userName + " / " + data.val().userName);
+        userArr.splice(i, 1);
+      }
+    });
+  };
+
+  fetchPosts(userInitial);
+
+  listeningFirebaseRefs.push(userInitial);
+}
+
+function findUIDItemInArr(item, userArray){
+  for(var i = 0; i < userArray.length; i++){
+    if(userArray[i].uid == item){
+      console.log("Found item: " + item);
+      return i;
+    }
+  }
+  return -1;
+}
 
 function login() {
   var validUserInt = 0;
