92e9b7e49a454f6c9cd7a4f688e4b00a610f0407
DonovanAdrian <websurfer21020@gmail.com>
2020-11-22 15:23:02 -0700

    Updated friendListAlg File
    Updated friendListAlg JS file with corrected open/closeModal updates.

--- src/js/friendListAlg.js
+++ src/js/friendListAlg.js
@@ -5,6 +5,7 @@ var inviteArr = [];
 var userUserNames = [];
 
 var areYouStillThereBool = false;
+var areYouStillThereInit = false;
 var readNotificationsBool = false;
 var updateGiftToDBBool = false;
 var giftListEmptyBool = false;
@@ -257,17 +258,20 @@ window.onload = function instantiate() {
             timeSecs = ("0" + timeSecs).slice(-2);
         }
 
-
-        closeModal(modal);
+        if(!areYouStillThereInit) {
+            closeModal(modal);
+            openModal(noteModal, "noteModal");
+            areYouStillThereInit = true;
+        }
         noteInfoField.innerHTML = "You have been inactive for 5 minutes, you will be logged out in " + timeMins
             + ":" + timeSecs + "!";
         noteTitleField.innerHTML = "Are You Still There?";
-        openModal(noteModal, "noteModal");
 
         //close on close
         noteSpan.onclick = function() {
             closeModal(noteModal);
             areYouStillThereBool = false;
+            areYouStillThereInit = false;
         };
     }
 
@@ -281,6 +285,7 @@ window.onload = function instantiate() {
             if(nowJ >= 3000){
                 closeModal(noteModal);
                 areYouStillThereBool = false;
+                areYouStillThereInit = false;
                 clearInterval(j);
             }
         }, 1000);
@@ -290,6 +295,7 @@ window.onload = function instantiate() {
             if (event.target == noteModal) {
                 closeModal(noteModal);
                 areYouStillThereBool = false;
+                areYouStillThereInit = false;
             }
         };
     }
