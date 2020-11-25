564d6c5d1b992afad870d0504363556060859be6
DonovanAdrian <websurfer21020@gmail.com>
2020-11-24 18:17:45 -0700

    Updated common File
    Updated common JS file with simplified navigation function as well as added signOut function.

--- src/js/common.js
+++ src/js/common.js
@@ -1,53 +1,35 @@
 var currentModalOpen = "";
 
-function newNavigation(num) {
-    switch(num){
-        case 1:
-            window.location.href = "index.html";
-            break;
-        case 2:
-            window.location.href = "home.html";
-            break;
-        case 3:
-            window.location.href = "lists.html";
-            break;
-        case 4:
-            window.location.href = "invites.html";
-            break;
-        case 5:
-            window.location.href = "settings.html";
-            break;
-        case 6:
-            window.location.href = "notifications.html";
-            break;
-        case 7:
-            window.location.href = "boughtGifts.html";
-            break;
-        case 8:
-            window.location.href = "giftAddUpdate.html";
-            break;
-        case 9:
-            window.location.href = "friendList.html";
-            break;
-        case 10:
-            window.location.href = "privateFriendList.html";
-            break;
-        case 11:
-            window.location.href = "confirmation.html";
-            break;
-        case 12:
-            window.location.href = "faq.html";
-            break;
-        case 13:
-            window.location.href = "userAddUpdate.html";
-            break;
-        case 14:
-            window.location.href = "moderation.html";
-            break;
-        default:
-            window.location.href = "404.html";
-            break;
-    }
+function signOut(){
+    sessionStorage.clear();
+    window.location.href = "index.html";
+}
+
+function newNavigation(navNum) {
+    sessionStorage.setItem("validUser", JSON.stringify(user));
+    sessionStorage.setItem("userArr", JSON.stringify(userArr));
+    var navLocations = [
+        "404.html",//0
+        "index.html",//1
+        "home.html",//2
+        "lists.html",//3
+        "invites.html",//4
+        "settings.html",//5
+        "notifications.html",//6
+        "boughtGifts.html",//7
+        "giftAddUpdate.html",//8
+        "friendList.html",//9
+        "privateFriendList.html",//10
+        "confirmation.html", //11
+        "faq.html",//12
+        "userAddUpdate.html",//13
+        "moderation.html"];//14
+
+    if (navNum >= navLocations.length)
+        navNum = 0;
+
+    console.log("Navigating to " + navLocations[navNum]);
+    window.location.href = navLocations[navNum];
 }
 
 function openModal(openThisModal, modalName){
