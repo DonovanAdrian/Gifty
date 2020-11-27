c91776080c1399cf555b580e8176302921af3ba6
DonovanAdrian <websurfer21020@gmail.com>
2020-11-26 21:54:24 -0700

    Updated confirmationAlg File
    Updated confirmationAlg JS file with support for newNavigation function

--- src/js/confirmationAlg.js
+++ src/js/confirmationAlg.js
@@ -58,18 +58,18 @@ function getCurrentUser(){
                 if (user.notifications.length > 0 && user.readNotifications.length != user.notifications.length) {
                     notificationBtn.src = "img/bellNotificationOn.png";
                     notificationBtn.onclick = function() {
-                        navigation(5);
+                        newNavigation(6);//Notifications
                     }
                 } else {
                     notificationBtn.src = "img/bellNotificationOff.png";
                     notificationBtn.onclick = function() {
-                        navigation(5);
+                        newNavigation(6);//Notifications
                     }
                 }
             } else if (user.notifications.length > 0) {
                 notificationBtn.src = "img/bellNotificationOn.png";
                 notificationBtn.onclick = function() {
-                    navigation(5);
+                    newNavigation(6);//Notifications
                 }
             }
         }
@@ -587,7 +587,7 @@ window.onload = function instantiate() {
             });
 
             if(inviteArr.length == 0)
-                navigation(2);
+                newNavigation(4);//Invites
         }
     }
 };
@@ -606,36 +606,4 @@ function deployInviteListEmptyNotification(){
     }
 
     clearInterval(offlineTimer);
-}
-
-function signOut(){
-    sessionStorage.clear();
-    window.location.href = "index.html";
-}
-
-function navigation(nav){
-    sessionStorage.setItem("validUser", JSON.stringify(user));
-    sessionStorage.setItem("userArr", JSON.stringify(userArr));
-    switch(nav){
-        case 0:
-            window.location.href = "home.html";
-            break;
-        case 1:
-            window.location.href = "lists.html";
-            break;
-        case 2:
-            window.location.href = "invites.html";
-            break;
-        case 3:
-            window.location.href = "settings.html";
-            break;
-        case 4:
-            window.location.href = "confirmation.html";
-            break;
-        case 5:
-            window.location.href = "notifications.html";
-            break;
-        default:
-            break;
-    }
 }
