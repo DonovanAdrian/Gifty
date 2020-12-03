ded1ca3e86894b1e74b096a6b3c21e4faeae9dde
DonovanAdrian <websurfer21020@gmail.com>
2020-12-02 19:18:48 -0700

    Updated faqAlg File
    Updated faqAlg JS file with commonInitialization support.

--- src/js/faqAlg.js
+++ src/js/faqAlg.js
@@ -1,12 +1,6 @@
 var userArr = [];
 var supportArr = [];
 
-var areYouStillThereBool = false;
-var areYouStillThereInit = false;
-
-var logoutReminder = 300;
-var logoutLimit = 900;
-
 var offlineSpan;
 var offlineModal;
 var emailBtn;
@@ -20,10 +14,11 @@ var noteSpan;
 var modal;
 
 
+
 function getCurrentUser(){
     try {
         user = JSON.parse(sessionStorage.validUser);
-        console.log("User: " + user.userName + " logged in");
+        console.log("User: " + user.userName + " loaded in");
         if (user.invites == undefined) {
             console.log("Invites Not Found");
         } else if (user.invites != undefined) {
@@ -51,53 +46,7 @@ window.onload = function instantiate() {
     noteSpan = document.getElementById('closeNotification');
     modal = document.getElementById('myModal');
     getCurrentUser();
-
-    const config = JSON.parse(sessionStorage.config);
-
-    firebase.initializeApp(config);
-    firebase.analytics();
-
-    firebase.auth().signInAnonymously().catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-    });
-
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-        } else {
-            // User is signed out.
-        }
-    });
-
-
-    window.addEventListener("online", function () {
-        closeModal(offlineModal);
-        location.reload();
-    });
-
-    window.addEventListener("offline", function () {
-        var now = 0;
-        offlineTimer = setInterval(function(){
-            now = now + 1000;
-            if(now >= 5000){
-                openModal(offlineModal, "offlineModal");
-                clearInterval(offlineTimer);
-            }
-        }, 1000);
-    });
-
-    offlineSpan.onclick = function () {
-        closeModal(offlineModal);
-    };
-
-    window.onclick = function (event) {
-        if (event.target == offlineModal) {
-            closeModal(offlineModal);
-        }
-    };
+    commonInitialization();
 
     emailBtn.onclick = function () {
         var supportStr = genSupport();
@@ -138,91 +87,6 @@ window.onload = function instantiate() {
     }
 
     settingsFAQButton();
-
-    loginTimer(); //if action, then reset timer
-
-    function loginTimer(){
-        var loginNum = 0;
-        console.log("Login Timer Started");
-        setInterval(function(){ //900 15 mins, 600 10 mins
-            document.onmousemove = resetTimer;
-            document.onkeypress = resetTimer;
-            document.onload = resetTimer;
-            document.onmousemove = resetTimer;
-            document.onmousedown = resetTimer; // touchscreen presses
-            document.ontouchstart = resetTimer;
-            document.onclick = resetTimer;     // touchpad clicks
-            document.onscroll = resetTimer;    // scrolling with arrow keys
-            document.onkeypress = resetTimer;
-            loginNum = loginNum + 1;
-            if (loginNum >= logoutLimit){//default 900
-                console.log("User Timed Out");
-                signOut();
-            } else if (loginNum > logoutReminder){//default 600
-                //console.log("User Inactive");
-                areYouStillThereNote(loginNum);
-                areYouStillThereBool = true;
-            }
-            function resetTimer() {
-                if (areYouStillThereBool) {
-                    //console.log("User Active");
-                    ohThereYouAre();
-                }
-                loginNum = 0;
-            }
-        }, 1000);
-    }
-
-    function areYouStillThereNote(timeElapsed){
-        var timeRemaining = logoutLimit - timeElapsed;
-        var timeMins = Math.floor(timeRemaining/60);
-        var timeSecs = timeRemaining%60;
-
-        if (timeSecs < 10) {
-            timeSecs = ("0" + timeSecs).slice(-2);
-        }
-
-        if(!areYouStillThereInit) {
-            closeModal(modal);
-            openModal(noteModal, "noteModal");
-            areYouStillThereInit = true;
-        }
-        noteInfoField.innerHTML = "You have been inactive for 5 minutes, you will be logged out in " + timeMins
-            + ":" + timeSecs + "!";
-        noteTitleField.innerHTML = "Are You Still There?";
-
-        //close on close
-        noteSpan.onclick = function() {
-            closeModal(noteModal);
-            areYouStillThereBool = false;
-            areYouStillThereInit = false;
-        };
-    }
-
-    function ohThereYouAre(){
-        noteInfoField.innerHTML = "Welcome back, " + user.name;
-        noteTitleField.innerHTML = "Oh, There You Are!";
-
-        var nowJ = 0;
-        var j = setInterval(function(){
-            nowJ = nowJ + 1000;
-            if(nowJ >= 3000){
-                closeModal(noteModal);
-                areYouStillThereBool = false;
-                areYouStillThereInit = false;
-                clearInterval(j);
-            }
-        }, 1000);
-
-        //close on click
-        window.onclick = function(event) {
-            if (event.target == noteModal) {
-                closeModal(noteModal);
-                areYouStillThereBool = false;
-                areYouStillThereInit = false;
-            }
-        };
-    }
 
     function settingsFAQButton(){
         var nowConfirm = 0;
