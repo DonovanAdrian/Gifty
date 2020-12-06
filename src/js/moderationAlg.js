6120d96aa9bb961e81d6fc3a7b8d4b4de0dddab2
DonovanAdrian <websurfer21020@gmail.com>
2020-12-05 20:41:17 -0700

    Updated moderationAlg File
    Updated moderationAlg JS file with support for commonInitialization function and dataListContainer.

--- src/js/moderationAlg.js
+++ src/js/moderationAlg.js
@@ -5,8 +5,6 @@ var userUIDArr = [];
 var tempUserArr = [];
 var optInUserArr = [];
 
-var areYouStillThereBool = false;
-var areYouStillThereInit = false;
 var secretSantaIntBool = false;
 var secretSantaNameBool = false;
 
@@ -14,8 +12,6 @@ var moderationSet = 1;
 var userCounter = 0;
 var onlineInt = 0;
 var loadingTimerInt = 0;
-var logoutReminder = 300;
-var logoutLimit = 1800;
 
 var giftList;
 var offlineSpan;
@@ -67,7 +63,7 @@ function getCurrentUser(){
 
 window.onload = function instantiate() {
 
-    giftList = document.getElementById('giftListContainer');
+    giftList = document.getElementById('dataListContainer');
     offlineModal = document.getElementById('offlineModal');
     offlineSpan = document.getElementById('closeOffline');
     inviteNote = document.getElementById('inviteNote');
@@ -85,76 +81,7 @@ window.onload = function instantiate() {
     secretSantaShuffle = document.getElementById('secretSantaShuffle');
     secretSantaBtn = document.getElementById('secretSantaBtn');
     getCurrentUser();
-
-    const config = JSON.parse(sessionStorage.config);
-
-    firebase.initializeApp(config);
-    firebase.analytics();
-
-    firebase.auth().signInAnonymously().catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-    });
-
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-        } else {
-            // User is signed out.
-        }
-    });
-
-
-    window.addEventListener("online", function(){
-        closeModal(offlineModal);
-        location.reload();
-    });
-
-    window.addEventListener("offline", function() {
-        var now = 0;
-        offlineTimer = setInterval(function(){
-            now = now + 1000;
-            if(now >= 5000){
-                try{
-                    if (onlineInt == 0) {
-                        document.getElementById("TestGift").innerHTML = "Loading Failed, Please Connect To Internet";
-                    } else {
-                        document.getElementById("TestGift").innerHTML = "No Users Found!";
-                    }
-                } catch(err) {
-                    if(userCounter == 0){
-                        console.log("Loading Element Missing, Creating A New One");
-                        var liItem = document.createElement("LI");
-                        liItem.id = "TestGift";
-                        liItem.className = "gift";
-                        if (onlineInt == 0) {
-                            var textNode = document.createTextNode("Loading Failed, Please Connect To Internet");
-                        } else {
-                            var textNode = document.createTextNode("No Users Found!");
-                        }
-                        liItem.appendChild(textNode);
-                        giftList.insertBefore(liItem, document.getElementById("giftListContainer").childNodes[0]);
-                    }
-                }
-                openModal(offlineModal, "offlineModal");
-                clearInterval(offlineTimer);
-            }
-        }, 1000);
-    });
-
-    //close offlineModal on close
-    offlineSpan.onclick = function() {
-        closeModal(offlineModal);
-    };
-
-    //close offlineModal on click
-    window.onclick = function(event) {
-        if (event.target == offlineModal) {
-            closeModal(offlineModal);
-        }
-    };
+    commonInitialization();
 
     loadingTimer = setInterval(function(){
         loadingTimerInt = loadingTimerInt + 1000;
@@ -172,8 +99,6 @@ window.onload = function instantiate() {
     databaseQuery();
 
     settingsModerateButton();
-
-    loginTimer(); //if action, then reset timer
 
     initializeSecretSantaArrs();
 
@@ -359,89 +284,6 @@ window.onload = function instantiate() {
                 console.log("Failed To Update Name " + userArr[i].name);
             }
         }
-    }
-
-    function loginTimer(){
-        var loginNum = 0;
-        console.log("Login Timer Started");
-        setInterval(function(){ //900 15 mins, 600 10 mins
-            document.onmousemove = resetTimer;
-            document.onkeypress = resetTimer;
-            document.onload = resetTimer;
-            document.onmousemove = resetTimer;
-            document.onmousedown = resetTimer; // touchscreen presses
-            document.ontouchstart = resetTimer;
-            document.onclick = resetTimer;     // touchpad clicks
-            document.onscroll = resetTimer;    // scrolling with arrow keys
-            document.onkeypress = resetTimer;
-            loginNum = loginNum + 1;
-            if (loginNum >= logoutLimit){//default 900
-                console.log("User Timed Out");
-                signOut();
-            } else if (loginNum > logoutReminder){//default 600
-                //console.log("User Inactive");
-                areYouStillThereNote(loginNum);
-                areYouStillThereBool = true;
-            }
-            function resetTimer() {
-                if (areYouStillThereBool) {
-                    //console.log("User Active");
-                    ohThereYouAre();
-                }
-                loginNum = 0;
-            }
-        }, 1000);
-    }
-
-    function areYouStillThereNote(timeElapsed){
-        var timeRemaining = logoutLimit - timeElapsed;
-        var timeMins = Math.floor(timeRemaining/60);
-        var timeSecs = timeRemaining%60;
-
-        if (timeSecs < 10) {
-            timeSecs = ("0" + timeSecs).slice(-2);
-        }
-
-        if(!areYouStillThereInit) {
-            closeModal(modal);
-            openModal(noteModal, "noteModal");
-            areYouStillThereInit = true;
-        }
-        noteInfoField.innerHTML = "You have been inactive for 5 minutes, you will be logged out in " + timeMins
-            + ":" + timeSecs + "!";
-        noteTitleField.innerHTML = "Are You Still There?";
-
-        //close on close
-        noteSpan.onclick = function() {
-            closeModal(noteModal);
-            areYouStillThereBool = false;
-            areYouStillThereInit = false;
-        };
-    }
-
-    function ohThereYouAre(){
-        noteInfoField.innerHTML = "Welcome back, " + user.name;
-        noteTitleField.innerHTML = "Oh, There You Are!";
-
-        var nowJ = 0;
-        var j = setInterval(function(){
-            nowJ = nowJ + 1000;
-            if(nowJ >= 3000){
-                closeModal(noteModal);
-                areYouStillThereBool = false;
-                areYouStillThereInit = false;
-                clearInterval(j);
-            }
-        }, 1000);
-
-        //close on click
-        window.onclick = function(event) {
-            if (event.target == noteModal) {
-                closeModal(noteModal);
-                areYouStillThereBool = false;
-                areYouStillThereInit = false;
-            }
-        };
     }
 
     function settingsModerateButton(){
@@ -834,7 +676,7 @@ window.onload = function instantiate() {
         var textNode = document.createTextNode(userData.name);
         liItem.appendChild(textNode);
 
-        giftList.insertBefore(liItem, document.getElementById("giftListContainer").childNodes[0]);
+        giftList.insertBefore(liItem, document.getElementById("dataListContainer").childNodes[0]);
         clearInterval(offlineTimer);
 
         userCounter++;
@@ -1021,7 +863,7 @@ function deployUserListEmptyNotification(){
         liItem.className = "gift";
         var textNode = document.createTextNode("No Users Found!");
         liItem.appendChild(textNode);
-        giftList.insertBefore(liItem, document.getElementById("giftListContainer").childNodes[0]);
+        giftList.insertBefore(liItem, document.getElementById("dataListContainer").childNodes[0]);
     }
 
     clearInterval(offlineTimer);
