0d0ce027f54f7de9f740ea9419d93d0f1cff6d06
DonovanAdrian <websurfer21020@gmail.com>
2020-12-08 17:57:02 -0700

    Updated userAddUpdateAlg File
    Updated userAddUpdateAlg JS file with support for commonInitialization function.

--- src/js/userAddUpdateAlg.js
+++ src/js/userAddUpdateAlg.js
@@ -5,12 +5,8 @@ var userData = [];
 var userArr = [];
 
 var userNameBool = true;
-var areYouStillThereBool = false;
-var areYouStillThereInit = false;
 
 var pinClearedInt = 0;
-var logoutReminder = 300;
-var logoutLimit = 900;
 
 var offlineSpan;
 var offlineModal;
@@ -52,90 +48,6 @@ function getCurrentUser(){
         btnDelete.innerHTML = "Loading...";
         userArr = JSON.parse(sessionStorage.userArr);
 
-        loginTimer(); //if action, then reset timer
-
-        function loginTimer(){
-            var loginNum = 0;
-            console.log("Login Timer Started");
-            setInterval(function(){ //900 15 mins, 600 10 mins
-                document.onmousemove = resetTimer;
-                document.onkeypress = resetTimer;
-                document.onload = resetTimer;
-                document.onmousemove = resetTimer;
-                document.onmousedown = resetTimer; // touchscreen presses
-                document.ontouchstart = resetTimer;
-                document.onclick = resetTimer;     // touchpad clicks
-                document.onscroll = resetTimer;    // scrolling with arrow keys
-                document.onkeypress = resetTimer;
-                loginNum = loginNum + 1;
-                if (loginNum >= logoutLimit){//default 900
-                    console.log("User Timed Out");
-                    signOut();
-                } else if (loginNum > logoutReminder){//default 600
-                    //console.log("User Inactive");
-                    areYouStillThereNote(loginNum);
-                    areYouStillThereBool = true;
-                }
-                function resetTimer() {
-                    if (areYouStillThereBool) {
-                        //console.log("User Active");
-                        ohThereYouAre();
-                    }
-                    loginNum = 0;
-                }
-            }, 1000);
-        }
-
-        function areYouStillThereNote(timeElapsed){
-            var timeRemaining = logoutLimit - timeElapsed;
-            var timeMins = Math.floor(timeRemaining/60);
-            var timeSecs = timeRemaining%60;
-
-            if (timeSecs < 10) {
-                timeSecs = ("0" + timeSecs).slice(-2);
-            }
-
-            if(!areYouStillThereInit) {
-                closeModal(modal);
-                openModal(noteModal, "noteModal");
-                areYouStillThereInit = true;
-            }
-            noteInfoField.innerHTML = "You have been inactive for 5 minutes, you will be logged out in " + timeMins
-                + ":" + timeSecs + "!";
-            noteTitleField.innerHTML = "Are You Still There?";
-
-            //close on close
-            noteSpan.onclick = function() {
-                closeModal(noteModal);
-                areYouStillThereBool = false;
-                areYouStillThereInit = false;
-            };
-        }
-
-        function ohThereYouAre(){
-            noteInfoField.innerHTML = "Welcome back, " + user.name;
-            noteTitleField.innerHTML = "Oh, There You Are!";
-
-            var nowJ = 0;
-            var j = setInterval(function(){
-                nowJ = nowJ + 1000;
-                if(nowJ >= 3000){
-                    closeModal(noteModal);
-                    areYouStillThereBool = false;
-                    areYouStillThereInit = false;
-                    clearInterval(j);
-                }
-            }, 1000);
-
-            //close on click
-            window.onclick = function(event) {
-                if (event.target == noteModal) {
-                    closeModal(noteModal);
-                    areYouStillThereBool = false;
-                    areYouStillThereInit = false;
-                }
-            };
-        }
         alert("Please note that you will be required to input your confirmation pin to continue. If you would like to " +
             "cancel, please click the back button on the browser.");
     }
@@ -161,55 +73,7 @@ window.onload = function instantiate() {
     noteInfoField = document.getElementById('notificationInfo');
     noteSpan = document.getElementById('closeNotification');
     getCurrentUser();
-
-    const config = JSON.parse(sessionStorage.config);
-
-    firebase.initializeApp(config);
-    firebase.analytics();
-
-    firebase.auth().signInAnonymously().catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-    });
-
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // User is signed in.
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-        } else {
-            // User is signed out.
-        }
-    });
-
-
-    window.addEventListener("online", function(){
-        closeModal(offlineModal);
-        location.reload();
-    });
-
-    window.addEventListener("offline", function() {
-        var now = 0;
-        var g = setInterval(function(){
-            now = now + 1000;
-            if(now >= 5000){
-                openModal(offlineModal, "offlineModal");
-                clearInterval(g);
-            }
-        }, 1000);
-    });
-
-    //close offlineModal on close
-    offlineSpan.onclick = function() {
-        closeModal(offlineModal);
-    };
-
-    //close offlineModal on click
-    window.onclick = function(event) {
-        if (event.target == offlineModal) {
-            closeModal(offlineModal);
-        }
-    };
+    commonInitialization();
 
     pinField.onclick = function() {
         if(pinClearedInt == 0) {
