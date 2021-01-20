6cad14b9f102d7af4359c6bc7f8148c0213e0aed
Donovan Adrian <websurfer21020@gmail.com>
2021-01-19 19:19:31 -0700

    Added familyUpdateAlg File
    Finally added the familyUpdateAlg JS file. It should be noted that this isn't very useful yet, since it has essentially the same code as the familyAlg JS file. However, it will eventually be brought up to proper working order!

---
+++ src/js/familyUpdateAlg.js
@@ -0,0 +1,480 @@
+/*
+ * This project was created by Donovan Adrian and any
+ * edits or changes must be confirmed as valid by Donovan
+ * with written consent under any circumstance.
+ */
+
+let familyUpdateElements = [];
+let listeningFirebaseRefs = [];
+let inviteArr = [];
+let userArr = [];
+let familyArr = [];
+let familyMemberArr = [];
+let familyConnectionArr = [];
+
+let moderationSet = 1;
+let onlineInt = 0;
+let familyCounter = 0;
+let loadingTimerInt = 0;
+
+let inviteNote;
+let settingsNote;
+let dataListContainer;
+let testData;
+let addMember;
+let familySettings;
+let familyAddModal;
+let closeFamilyAddModal;
+let familyMemberInp;
+let addFamilyMember;
+let cancelFamilyMember;
+let familyNameModal;
+let closeFamilyNameModal;
+let familyNameInp;
+let updateFamilyName;
+let cancelFamilyName;
+let familyLinkModal;
+let closeFamilyLinkModal;
+let familyLinkInp;
+let addFamilyLink;
+let cancelFamilyLink;
+let familySettingsModal;
+let closeFamilySettings;
+let changeFamilyName;
+let linkFamilies;
+let offlineModal;
+let offlineSpan;
+let notificationModal;
+let noteSpan;
+let notificationTitle;
+let notificationInfo;
+let offlineTimer;
+let loadingTimer;
+let userInitial;
+let userInvites;
+let familyInitial;
+//-----------------------**************************************ToDo
+
+
+
+function getCurrentUser(){
+  try {
+    user = JSON.parse(sessionStorage.validUser);
+    console.log("User: " + user.userName + " loaded in");
+    if (user.invites == undefined) {
+      console.log("Invites Not Found");
+    } else if (user.invites != undefined) {
+      if (user.invites.length > 0) {
+        inviteNote.style.background = "#ff3923";
+      }
+    }
+
+    if (user.moderatorInt == 0){
+      window.location.href = "home.html";
+    }
+    userArr = JSON.parse(sessionStorage.userArr);
+    sessionStorage.setItem("moderationSet", moderationSet);
+  } catch (err) {
+    console.log(err.toString());
+    window.location.href = "index.html";
+  }
+}
+
+window.onload = function instantiate() {
+
+  inviteNote = document.getElementById('inviteNote');
+  settingsNote = document.getElementById('settingsNote');
+  dataListContainer = document.getElementById('dataListContainer');
+  testData = document.getElementById('testData');
+  //-----------------------**************************************ToDo
+  familyUpdateElements = [
+
+  ];
+  verifyElementIntegrity(familyUpdateElements);
+  getCurrentUser();
+  commonInitialization();
+
+  userInitial = firebase.database().ref("users/");
+  userInvites = firebase.database().ref("users/" + user.uid + "/invites");
+  familyInitial = firebase.database().ref("family/");
+
+  loadingTimer = setInterval(function(){
+    loadingTimerInt = loadingTimerInt + 1000;
+    if(loadingTimerInt >= 2000){
+      if(loadingTimerInt >= 5000){
+        clearInterval(loadingTimer);
+        if (testData == undefined) {
+          //console.log("testGift Missing. Loading Properly.");
+        } else {
+          deployFamilyListEmptyNotification();
+        }
+      } else {
+        if (testData == undefined) {
+          //console.log("testGift Missing. Loading Properly.");
+        } else {
+          testData.innerHTML = "Loading... Please Wait...";
+        }
+      }
+    }
+  }, 1000);
+
+  databaseQuery();
+
+  familyModerateButton();
+
+  createFamilyBtn.innerHTML = "Create Family";
+
+  createFamilyBtn.onclick = function() {
+    generateAddFamilyModal();//-----------------------**************************************ToDo
+  };
+
+  function generateAddFamilyModal(){
+    addFamily.onclick = function() {
+      if(familyNameInp.value != "" || (familyNameInp.value.includes(" ") && isAlph(familyNameInp.value.charAt(0))))
+        addFamilyMemberToDB(familyNameInp.value);
+    };
+
+    cancelFamily.onclick = function() {
+      familyNameInp.value = "";
+      closeModal(familyAddModal);
+    };
+
+    openModal(familyAddModal, "familyAddModal");
+
+    closeFamilyAddModal.onclick = function() {
+      closeModal(familyAddModal);
+    };
+
+    window.onclick = function(event) {
+      if (event.target == familyAddModal) {
+        closeModal(familyAddModal);
+      }
+    };
+  }
+
+  function isAlph(rChar){
+    rChar = rChar.toUpperCase();
+    switch (rChar){
+      case "A":
+      case "B":
+      case "C":
+      case "D":
+      case "E":
+      case "F":
+      case "G":
+      case "H":
+      case "I":
+      case "J":
+      case "K":
+      case "L":
+      case "M":
+      case "N":
+      case "O":
+      case "P":
+      case "Q":
+      case "R":
+      case "S":
+      case "T":
+      case "U":
+      case "V":
+      case "W":
+      case "X":
+      case "Y":
+      case "Z":
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  function familyModerateButton(){
+    let nowConfirm = 0;
+    let alternator = 0;
+    console.log("Settings Button Feature Active");
+    setInterval(function(){
+      nowConfirm = nowConfirm + 1000;
+      if(nowConfirm >= 3000){
+        nowConfirm = 0;
+        if(alternator == 0) {
+          alternator++;
+          settingsNote.innerHTML = "Settings";
+          settingsNote.style.background = "#00c606";
+        } else {
+          alternator--;
+          settingsNote.innerHTML = "Family";
+          settingsNote.style.background = "#00ad05";
+        }
+      }
+    }, 1000);
+  }
+
+  function databaseQuery() {
+
+    let fetchData = function (postRef) {
+      postRef.on('child_added', function (data) {
+        let i = findUIDItemInArr(data.key, userArr);
+        if(userArr[i] != data.val() && i != -1)
+          userArr[i] = data.val();
+
+        if(data.key == user.uid)
+          user = data.val();
+      });
+
+      postRef.on('child_changed', function (data) {
+        let i = findUIDItemInArr(data.key, userArr);
+        if(userArr[i] != data.val() && i != -1)
+          userArr[i] = data.val();
+
+        if(data.key == user.uid)
+          user = data.val();
+      });
+
+      postRef.on('child_removed', function (data) {
+        let i = findUIDItemInArr(data.key, userArr);
+        if(userArr[i] != data.val() && i != -1)
+          userArr.splice(i, 1);
+      });
+    };
+
+    let fetchInvites = function (postRef) {
+      postRef.on('child_added', function (data) {
+        inviteArr.push(data.val());
+
+        inviteNote.style.background = "#ff3923";
+      });
+
+      postRef.on('child_changed', function (data) {
+        inviteArr[data.key] = data.val();
+      });
+
+      postRef.on('child_removed', function (data) {
+        inviteArr.splice(data.key, 1);
+
+        if (inviteArr.length == 0) {
+          console.log("Invite List Removed");
+          inviteNote.style.background = "#008222";
+        }
+      });
+    };
+
+    let fetchFamilies = function (postRef){
+      postRef.on('child_added', function (data) {
+        familyArr.push(data.val());
+
+        createFamilyElement(data.val());
+      });
+
+      postRef.on('child_changed', function (data) {
+        let i = findUIDItemInArr(data.key, familyArr);
+        if(familyArr[i] != data.val() && i != -1) {
+          familyArr[i] = data.val();
+          changeFamilyElement(data.val());
+        }
+      });
+
+      postRef.on('child_removed', function (data) {
+        let i = findUIDItemInArr(data.key, familyArr);
+        if(familyArr[i] != data.val() && i != -1) {
+          familyArr.splice(i, 1);
+          removeFamilyMemberElement(data.key);
+        }
+      });
+    };
+
+    fetchData(userInitial);
+    fetchInvites(userInvites);
+    fetchFamilies(familyInitial);
+
+    listeningFirebaseRefs.push(userInitial);
+    listeningFirebaseRefs.push(userInvites);
+    listeningFirebaseRefs.push(familyInitial);
+  }
+
+  function findUIDItemInArr(item, userArray){
+    for(let i = 0; i < userArray.length; i++)
+      if(userArray[i].uid == item)
+        return i;
+    return -1;
+  }
+
+  function createFamilyElement(familyData){
+    try{
+      testData.remove();
+    } catch (err) {}
+
+    let liItem = document.createElement("LI");
+    liItem.id = "family" + familyData.uid;
+    liItem.className = "gift";
+    liItem.onclick = function (){
+      familyConnectionArr = familyData.connections;
+      familyMemberArr = familyData.members;
+
+      if(familyConnectionArr != null)
+        familyConnectionCount.innerHTML = familyConnectionArr.length;
+
+      if(familyMemberArr != null) {
+        familyMemberCount.innerHTML = familyMemberArr.length;
+        try{
+          testFamily.remove();
+        } catch (err) {}
+        for(let i = 0; i < familyMemberArr.length; i++){
+          let liItem = document.createElement("LI");
+          let familyMember = findUIDItemInArr(familyMemberArr[i], userArr);
+          liItem.id = familyMemberArr[i];
+          liItem.className = "gift";
+          let textNode = document.createTextNode(userArr[familyMember].name);
+          liItem.appendChild(textNode);
+          dataListContainer.insertBefore(liItem, familyListContainer.childNodes[0]);
+        }
+      } else {
+        familyMemberCount.innerHTML = 0;
+        let liItem = document.createElement("LI");
+        liItem.id = "testFamily";
+        liItem.className = "gift";
+        let textNode = document.createTextNode("No Family Members Found!");
+        liItem.appendChild(textNode);
+        dataListContainer.insertBefore(liItem, familyListContainer.childNodes[0]);
+      }
+
+
+      familyEdit.onclick = function (){
+        sessionStorage.setItem("familyData", JSON.stringify(familyData));
+        newNavigation(16);
+      };
+
+      familyRemove.onclick = function (){
+        removeFamilyMemberFromDB(familyData.uid);
+      };
+
+      //show modal
+      openModal(familyModal, familyData.uid);
+
+      //close on close
+      spanGift.onclick = function() {
+        closeModal(familyModal);
+      };
+
+      //close on click
+      window.onclick = function(event) {
+        if (event.target == familyModal) {
+          closeModal(familyModal);
+        }
+      };
+    };
+    let textNode = document.createTextNode(familyData.name);
+    liItem.appendChild(textNode);
+
+    dataListContainer.insertBefore(liItem, dataListContainer.childNodes[0]);
+    clearInterval(offlineTimer);
+    familyCounter++;
+  }
+
+  function changeFamilyElement(familyData) {
+    let editGift = document.getElementById('family' + familyData.uid);
+    editGift.innerHTML = familyData.name;
+    editGift.className = "gift";
+    editGift.onclick = function (){
+      familyConnectionArr = familyData.connections;
+      familyMemberArr = familyData.members;
+
+      if(familyConnectionArr != null)
+        familyConnectionCount.innerHTML = familyConnectionArr.length;
+
+      if(familyMemberArr != null) {
+        familyMemberCount.innerHTML = familyMemberArr.length;
+        try{
+          testFamily.remove();
+        } catch (err) {}
+        for(let i = 0; i < familyMemberArr.length; i++){
+          let liItem = document.createElement("LI");
+          let familyMember = findUIDItemInArr(familyMemberArr[i], userArr);
+          liItem.id = familyMemberArr[i];
+          liItem.className = "gift";
+          let textNode = document.createTextNode(userArr[familyMember].name);
+          liItem.appendChild(textNode);
+          dataListContainer.insertBefore(liItem, familyListContainer.childNodes[0]);
+        }
+      } else {
+        familyMemberCount.innerHTML = 0;
+        let liItem = document.createElement("LI");
+        liItem.id = "testFamily";
+        liItem.className = "gift";
+        let textNode = document.createTextNode("No Family Members Found!");
+        liItem.appendChild(textNode);
+        dataListContainer.insertBefore(liItem, familyListContainer.childNodes[0]);
+      }
+
+
+      familyEdit.onclick = function (){
+        sessionStorage.setItem("familyData", JSON.stringify(familyData));
+        newNavigation(16);
+      };
+
+      familyRemove.onclick = function (){
+        removeFamilyMemberFromDB(familyData.uid);
+      };
+
+      //show modal
+      openModal(familyModal, familyData.uid);
+
+      //close on close
+      spanGift.onclick = function() {
+        closeModal(familyModal);
+      };
+
+      //close on click
+      window.onclick = function(event) {
+        if (event.target == familyModal) {
+          closeModal(familyModal);
+        }
+      };
+    };
+  }
+
+  function removeFamilyMemberFromDB(uid) {
+    alert("This will eventually remove the family data from the database");
+    //confirm
+    //firebase.database().ref("family/").child(uid).remove();
+  }
+
+
+  function removeFamilyMemberElement(uid) {
+    document.getElementById('family' + uid).remove();
+
+    familyCounter--;
+    if (familyCounter == 0){
+      deployFamilyListEmptyNotification();
+    }
+  }
+
+  function addFamilyMemberToDB(familyName){
+    let newUid = firebase.database().ref("family").push();
+    newUid = newUid.toString();
+    //newUid = newUid.substr(51, 70);
+    console.log(newUid);
+    console.log(familyName);
+
+    /*
+    firebase.database().ref("family/" + newUid).set({
+      uid: newUid,
+      name: familyName
+    });
+    */
+  }
+};
+
+function deployFamilyListEmptyNotification(){
+  try{
+    testData.innerHTML = "No Families Found!";
+  } catch(err){
+    console.log("Loading Element Missing, Creating A New One");
+    let liItem = document.createElement("LI");
+    liItem.id = "testGift";
+    liItem.className = "gift";
+    let textNode = document.createTextNode("No Families Found!");
+    liItem.appendChild(textNode);
+    dataListContainer.insertBefore(liItem, dataListContainer.childNodes[0]);
+  }
+
+  clearInterval(offlineTimer);
+}
