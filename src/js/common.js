3a446f541343bfeb6b30e842a0e8242016f18efb
Donovan Adrian <websurfer21020@gmail.com>
2021-03-03 22:55:38 -0700

    Updated common File
    Updated common JS file with moved isAlph() function. I plan to move the alternator button function here as well once I adjust the code to be more dynamic for each page.

--- src/js/common.js
+++ src/js/common.js
@@ -286,3 +286,38 @@ function closeModal(closeThisModal){
       console.log("Modal Not Open");
   }
 }
+
+function isAlph(rChar){
+  rChar = rChar.toUpperCase();
+  switch (rChar){
+    case "A":
+    case "B":
+    case "C":
+    case "D":
+    case "E":
+    case "F":
+    case "G":
+    case "H":
+    case "I":
+    case "J":
+    case "K":
+    case "L":
+    case "M":
+    case "N":
+    case "O":
+    case "P":
+    case "Q":
+    case "R":
+    case "S":
+    case "T":
+    case "U":
+    case "V":
+    case "W":
+    case "X":
+    case "Y":
+    case "Z":
+      return true;
+    default:
+      return false;
+  }
+}
