c803fb06019e09d84645b89216a073a3db5ef2de
Donovan Adrian <websurfer21020@gmail.com>
2021-05-09 16:58:53 -0600

    Updated moderationQueueAlg File
    Updated moderationQueueAlg JS file with removal of large commented section and laying ground work for future updates on this page.

--- src/js/moderationQueueAlg.js
+++ src/js/moderationQueueAlg.js
@@ -11,7 +11,6 @@ let userArr = [];
 
 let moderationSet = 1;
 let dataCounter = 0;
-let onlineInt = 0;
 let loadingTimerInt = 0;
 
 let dataListContainer;
@@ -75,7 +74,7 @@ window.onload = function instantiate() {
     loadingTimerInt = loadingTimerInt + 1000;
     if(loadingTimerInt >= 2000){
       if (testData == undefined){
-        //console.log("TestData Missing. Loading Properly.");
+        console.log("TestData Missing. Loading Properly.");
       } else {
         testData.innerHTML = "Loading... Please Wait...";
       }
@@ -87,8 +86,6 @@ window.onload = function instantiate() {
 
   alternateButtonLabel(settingsNote, "Settings", "Moderation");
 
-  //Needs to be updated to apply to this page---------------------**************ToDo
-  /*
   function databaseQuery() {
 
     userInitial = firebase.database().ref("users/");
@@ -96,22 +93,15 @@ window.onload = function instantiate() {
 
     let fetchData = function (postRef) {
       postRef.on('child_added', function (data) {
-        createUserElement(data.val());
-
-        if(onlineInt == 0) {
-          onlineInt = 1;
-          initializeGlobalNotification();
-        }
-
         let i = findUIDItemInArr(data.key, userArr);
         if(userArr[i] != data.val() && i != -1){
-          //console.log("Adding " + userArr[i].userName + " to most updated version: " + data.val().userName);
+          console.log("Adding " + userArr[i].userName + " to most updated version: " + data.val().userName);
           userArr[i] = data.val();
         }
 
         if(data.key == user.uid){
           user = data.val();
-          //console.log("User Updated: 1");
+          console.log("User Updated: 1");
         }
       });
 
@@ -120,13 +110,13 @@ window.onload = function instantiate() {
 
         let i = findUIDItemInArr(data.key, userArr);
         if(userArr[i] != data.val() && i != -1){
-          //console.log("Updating " + userArr[i].userName + " to most updated version: " + data.val().userName);
+          console.log("Updating " + userArr[i].userName + " to most updated version: " + data.val().userName);
           userArr[i] = data.val();
         }
 
         if(data.key == user.uid){
           user = data.val();
-          //console.log("User Updated: 2");
+          console.log("User Updated: 2");
         }
 
         if(currentModalOpen == data.key) {//Moved currentModalOpen reference to common.js
@@ -139,7 +129,7 @@ window.onload = function instantiate() {
 
         let i = findUIDItemInArr(data.key, userArr);
         if(userArr[i] != data.val() && i != -1){
-          //console.log("Removing " + userArr[i].userName + " / " + data.val().userName);
+          console.log("Removing " + userArr[i].userName + " / " + data.val().userName);
           userArr.splice(i, 1);
         }
 
@@ -180,283 +170,4 @@ window.onload = function instantiate() {
     listeningFirebaseRefs.push(userInitial);
     listeningFirebaseRefs.push(userInvites);
   }
-   */
-
-  //Needs to be updated to apply to this page---------------------**************ToDo
-  /*
-  function createUserElement(userData){
-    try{
-      testData.remove();
-    } catch (err) {}
-
-    let liItem = document.createElement("LI");
-    liItem.id = "user" + userData.uid;
-    liItem.className = "gift";
-    if (userData.secretSanta != null)
-      if (userData.secretSanta == 1)
-        liItem.className += " santa";
-    liItem.onclick = function (){
-      userName.innerHTML = userData.name;
-      userUID.innerHTML = userData.uid;
-      userUserName.innerHTML = userData.userName;
-      if(userData.giftList != undefined){
-        userGifts.innerHTML = "# Gifts: " + userData.giftList.length;
-      } else {
-        userGifts.innerHTML = "This User Has No Gifts";
-      }
-      if(userData.privateList != undefined){
-        userPrivateGifts.innerHTML = "# Private Gifts: " + userData.privateList.length;
-      } else {
-        userPrivateGifts.innerHTML = "This User Has No Private Gifts";
-      }
-      if(userData.friends != undefined) {
-        userFriends.innerHTML = "# Friends: " + userData.friends.length;
-      } else {
-        userFriends.innerHTML = "This User Has No Friends";
-      }
-      userPassword.innerHTML = "Click On Me To View Password";
-
-      if(userData.secretSanta != undefined) {
-        if (userData.secretSanta == 0)
-          userSecretSanta.innerHTML = "This User Is Not Opted Into Secret Santa";
-        else
-          userSecretSanta.innerHTML = "This User Is Signed Up For Secret Santa";
-      } else {
-        userSecretSanta.innerHTML = "This User Is Not Opted Into Secret Santa";
-      }
-
-      userSecretSanta.onclick = function() {
-        manuallyOptInOut(userData);
-      };
-
-      userGifts.onclick = function() {
-        if(userData.uid == user.uid){
-          alert("Navigate to the home page to see your gifts!");
-        } else {
-          sessionStorage.setItem("validGiftUser", JSON.stringify(userData));//Other User Data
-          newNavigation(9);//FriendList
-        }
-      };
-      userPrivateGifts.onclick = function() {
-        if(userData.uid == user.uid){
-          alert("You aren't allowed to see these gifts, silly!");
-        } else {
-          sessionStorage.setItem("validGiftUser", JSON.stringify(userData));//Other User Data
-          newNavigation(10);//PrivateFriendList
-        }
-      };
-      userPassword.onclick = function() {
-        try {
-          userPassword.innerHTML = decode(userData.encodeStr);
-        } catch (err) {
-          userPassword.innerHTML = userData.pin;
-        }
-      };
-      warnUser.onclick = function(){
-        alert("This will eventually warn the user of a certain offense");
-        //warn function
-      };
-      banUser.onclick = function(){
-        alert("This will eventually ban the user for a certain offense");
-        //ban function
-      };
-      if (userData.uid == "-L__dcUyFssV44G9stxY" && user.uid != "-L__dcUyFssV44G9stxY") {
-        moderatorOp.innerHTML = "Don't Even Think About It";
-        moderatorOp.onclick = function() {
-
-        }
-      } else if (userData.moderatorInt == 1) {
-        moderatorOp.innerHTML = "Revoke Moderator Role";
-        moderatorOp.onclick = function() {
-          if(userData.uid == user.uid){
-            alert("You cannot adjust your own role");
-          } else {
-            alert("Revoked role for: " + userData.userName);
-            firebase.database().ref("users/" + userData.uid).update({
-              moderatorInt: 0
-            });
-            closeModal(userModal);
-          }
-        };
-      } else {
-        moderatorOp.innerHTML = "Grant Moderator Role";
-        moderatorOp.onclick = function() {
-          if(userData.userName == user.userName){
-            alert("You cannot adjust your own role");
-            console.log("...How'd you get here...?");
-          } else {
-            alert("Granted role for: " + userData.userName);
-            firebase.database().ref("users/" + userData.uid).update({
-              moderatorInt: 1
-            });
-            closeModal(userModal);
-          }
-        };
-      }
-
-      sendPrivateMessage.innerHTML = "Send Message To " + userData.name;
-      sendPrivateMessage.onclick = function() {
-        generatePrivateMessageDialog(userData);
-      };
-
-      //show modal
-      openModal(userModal, userData.uid);
-
-      //close on close
-      closeUserModal.onclick = function() {
-        closeModal(userModal);
-      };
-
-      //close on click
-      window.onclick = function(event) {
-        if (event.target == userModal) {
-          closeModal(userModal);
-        }
-      };
-    };
-    let textNode = document.createTextNode(userData.name);
-    liItem.appendChild(textNode);
-
-    dataListContainer.insertBefore(liItem, dataListContainer.childNodes[0]);
-    clearInterval(offlineTimer);
-
-    dataCounter++;
-    if (dataCounter > 5) {
-      activateSecretSanta.style.opacity = ".75";
-    }
-  }
-
-  function changeUserElement(userData) {
-    let editGift = document.getElementById('user' + userData.uid);
-    editGift.innerHTML = userData.name;
-    editGift.className = "gift";
-    if (userData.secretSanta != null)
-      if (userData.secretSanta == 1)
-        editGift.className += " santa";
-    editGift.onclick = function (){
-      userName.innerHTML = userData.name;
-      userUID.innerHTML = userData.uid;
-      userUserName.innerHTML = userData.userName;
-      if(userData.giftList != undefined){
-        userGifts.innerHTML = "# Gifts: " + userData.giftList.length;
-      } else {
-        userGifts.innerHTML = "This User Has No Gifts";
-      }
-      if(userData.privateList != undefined){
-        userPrivateGifts.innerHTML = "# Private Gifts: " + userData.privateList.length;
-      } else {
-        userPrivateGifts.innerHTML = "This User Has No Private Gifts";
-      }
-      if(userData.friends != undefined) {
-        userFriends.innerHTML = "# Friends: " + userData.friends.length;
-      } else {
-        userFriends.innerHTML = "This User Has No Friends";
-      }
-      userPassword.innerHTML = "Click On Me To View Password";
-
-      if(userData.secretSanta != undefined) {
-        if (userData.secretSanta == 0)
-          userSecretSanta.innerHTML = "This User Is Not Opted Into Secret Santa";
-        else
-          userSecretSanta.innerHTML = "This User Is Signed Up For Secret Santa";
-      } else {
-        userSecretSanta.innerHTML = "This User Is Not Opted Into Secret Santa";
-      }
-
-      userSecretSanta.onclick = function() {
-        manuallyOptInOut(userData);
-      };
-
-      userGifts.onclick = function() {
-        if(userData.uid == user.uid){
-          alert("Navigate to the home page to see your gifts!");
-        } else {
-          sessionStorage.setItem("validGiftUser", JSON.stringify(userData));//Other User Data
-          newNavigation(9);//FriendList
-        }
-      };
-      userPrivateGifts.onclick = function() {
-        if(userData.uid == user.uid){
-          alert("You aren't allowed to see these gifts, silly!");
-        } else {
-          sessionStorage.setItem("validGiftUser", JSON.stringify(userData));//Other User Data
-          newNavigation(10);//PrivateFriendList
-        }
-      };
-      userPassword.onclick = function() {
-        userPassword.innerHTML = decode(userData.encodeStr);
-      };
-      warnUser.onclick = function(){
-        alert("This will eventually warn the user of a certain offense");
-        //warn function
-      };
-      banUser.onclick = function(){
-        alert("This will eventually ban the user for a certain offense");
-        //ban function
-      };
-      if (userData.uid == "-L__dcUyFssV44G9stxY" && user.uid != "-L__dcUyFssV44G9stxY") {
-        moderatorOp.innerHTML = "Don't Even Think About It";
-        moderatorOp.onclick = function() {
-
-        }
-      } else if (userData.moderatorInt == 1) {
-        moderatorOp.innerHTML = "Revoke Moderator Role";
-        moderatorOp.onclick = function() {
-          if(userData.uid == user.uid){
-            alert("You cannot adjust your own role");
-          } else {
-            alert("Revoked role for: " + userData.userName);
-            firebase.database().ref("users/" + userData.uid).update({
-              moderatorInt: 0
-            });
-            closeModal(userModal);
-          }
-        };
-      } else {
-        moderatorOp.innerHTML = "Grant Moderator Role";
-        moderatorOp.onclick = function() {
-          if(userData.userName == user.userName){
-            alert("You cannot adjust your own role");
-            console.log("...How'd you get here...?");
-          } else {
-            alert("Granted role for: " + userData.userName);
-            firebase.database().ref("users/" + userData.uid).update({
-              moderatorInt: 1
-            });
-            closeModal(userModal);
-          }
-        };
-      }
-
-      sendPrivateMessage.innerHTML = "Send Message To " + userData.name;
-      sendPrivateMessage.onclick = function() {
-        generatePrivateMessageDialog(userData);
-      };
-
-      //show modal
-      openModal(userModal, userData.uid);
-
-      //close on close
-      closeUserModal.onclick = function() {
-        closeModal(userModal);
-      };
-
-      //close on click
-      window.onclick = function(event) {
-        if (event.target == userModal) {
-          closeModal(userModal);
-        }
-      };
-    };
-  }
-
-  function removeUserElement(uid) {
-    document.getElementById('user' + uid).remove();
-
-    dataCounter--;
-    if (dataCounter == 0){
-      deployListEmptyNotification("No Queue Items Found!");
-    }
-  }
-   */
 };
