f80ebd59f131704186d7ab941410244ba9217dd0
Donovan Adrian <websurfer21020@gmail.com>
2021-11-09 17:38:13 -0700

    Updated confirmationAlg File
    Updated confirmationAlg JS file with updated console log.

--- src/js/confirmationAlg.js
+++ src/js/confirmationAlg.js
@@ -165,7 +165,7 @@ window.onload = function instantiate() {
         if(data.key == user.uid){
           user = data.val();
           if(consoleOutput)
-            console.log("User Updated: 2");
+            console.log("Current User Updated");
         }
       });
 
