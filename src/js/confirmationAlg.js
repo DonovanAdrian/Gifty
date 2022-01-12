1e94ffc08f1e9638cb02796661c3cb846017bf84
Donovan Adrian <websurfer21020@gmail.com>
2022-01-11 20:12:24 -0700

    Updated confirmationAlg File
    Updated confirmationAlg JS file with change to display number of friends.

--- src/js/confirmationAlg.js
+++ src/js/confirmationAlg.js
@@ -63,6 +63,15 @@ function getCurrentUser(){
       }
     }
 
+    if (user.friends == undefined) {
+      if(localConsoleOutput)
+        console.log("Friends Not Found");
+    } else if (user.friends != undefined) {
+      if (user.friends.length < 100 && user.friends.length > 0) {
+        inviteNote.innerHTML = user.friends.length + " Friends";
+      }
+    }
+
     if (user.readNotifications == undefined) {
       if(localConsoleOutput)
         console.log("Read Notifications Not Found");
