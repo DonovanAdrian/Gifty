4b03fea4b573ed7858a25ae9233a46abc2b39502
Donovan Adrian <websurfer21020@gmail.com>
2022-01-24 20:40:07 -0700

    Updated notificationsAlg File
    Updated notificationsAlg JS file with change to see number of friends.

--- src/js/notificationsAlg.js
+++ src/js/notificationsAlg.js
@@ -65,7 +65,14 @@ function getCurrentUser(){
         inviteNote.style.background = "#ff3923";
       }
     }
-
+    if (user.friends == undefined) {
+      if(localConsoleOutput)
+        console.log("Friends Not Found");
+    } else if (user.friends != undefined) {
+      if (user.friends.length < 100 && user.friends.length > 0) {
+        inviteNote.innerHTML = user.friends.length + " Friends";
+      }
+    }
     if(localConsoleOutput)
       console.log(user.notifications);
     if(user.notifications == undefined) {
