b1bed898ab0a883051eac1395d759dae92273381
Donovan Adrian <websurfer21020@gmail.com>
2022-10-01 15:48:06 -0600

    Updated friendListAlg File
    Updated friendListAlg JS file with swapped flickerNotification function.

--- src/js/friendListAlg.js
+++ src/js/friendListAlg.js
@@ -81,7 +81,7 @@ function getCurrentUser(){
   } else if (user.notifications != undefined) {
     if (readNotificationsBool){
       if (user.notifications.length > 0 && user.readNotifications.length != user.notifications.length) {
-        notificationBtn.src = "img/bellNotificationOn.png";
+        flickerNotification();
         notificationBtn.onclick = function() {
           navigation(6);//Notifications
         }
@@ -92,7 +92,7 @@ function getCurrentUser(){
         }
       }
     } else if (user.notifications.length > 0) {
-      notificationBtn.src = "img/bellNotificationOn.png";
+      flickerNotification();
       notificationBtn.onclick = function() {
         navigation(6);//Notifications
       }
