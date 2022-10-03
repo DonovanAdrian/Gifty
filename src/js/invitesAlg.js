5f43cd2297bb19fd415b10f6d2a2d6c555be9042
Donovan Adrian <websurfer21020@gmail.com>
2022-10-02 19:29:19 -0600

    Updated invitesAlg File
    Updated invitesAlg JS file with swapped flickerNotification function.

--- src/js/invitesAlg.js
+++ src/js/invitesAlg.js
@@ -106,7 +106,7 @@ function getCurrentUser(){
   } else if (user.notifications != undefined) {
     if (readNotificationsBool){
       if (user.notifications.length > 0 && user.readNotifications.length != user.notifications.length) {
-        notificationBtn.src = "img/bellNotificationOn.png";
+        flickerNotification();
         notificationBtn.onclick = function() {
           navigation(6);//Notifications
         }
@@ -117,7 +117,7 @@ function getCurrentUser(){
         }
       }
     } else if (user.notifications.length > 0) {
-      notificationBtn.src = "img/bellNotificationOn.png";
+      flickerNotification();
       notificationBtn.onclick = function() {
         navigation(6);//Notifications
       }
