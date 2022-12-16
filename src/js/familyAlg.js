df0128fcf1533fe255e81f53bbc171761871131e
Donovan Adrian <websurfer21020@gmail.com>
2022-12-15 22:17:21 -0700

    Updated familyAlg File
    Updated familyAlg JS file with added findObjectChanges implementations and explicit testData removal.

--- src/js/familyAlg.js
+++ src/js/familyAlg.js
@@ -134,26 +134,46 @@ window.onload = function instantiate() {
     let fetchData = function (postRef) {
       postRef.on('child_added', function (data) {
         let i = findUIDItemInArr(data.key, userArr, true);
-        if(userArr[i] != data.val() && i != -1)
-          userArr[i] = data.val();
+        if (i != -1) {
+          if (findObjectChanges(userArr[i], data.val()).length != 0) {
+            userArr[i] = data.val();
 
-        if(data.key == user.uid)
-          user = data.val();
+            if (data.key == user.uid) {
+              user = data.val();
+            }
+          }
+        } else {
+          userArr.push(data.val());
+
+          if (data.key == user.uid) {
+            user = data.val();
+          }
+        }
       });
 
       postRef.on('child_changed', function (data) {
-        let i = findUIDItemInArr(data.key, userArr);
-        if(userArr[i] != data.val() && i != -1)
-          userArr[i] = data.val();
+        let i = findUIDItemInArr(data.key, userArr, true);
+        if (i != -1) {
+          if (findObjectChanges(userArr[i], data.val()).length != 0) {
+            if (consoleOutput)
+              console.log("Updating " + userArr[i].userName + " to most updated version: " + data.val().userName);
+            userArr[i] = data.val();
 
-        if(data.key == user.uid)
-          user = data.val();
+            if (data.key == user.uid) {
+              user = data.val();
+              updateFriendNav(user.friends);
+              if(consoleOutput)
+                console.log("Current User Updated");
+            }
+          }
+        }
       });
 
       postRef.on('child_removed', function (data) {
         let i = findUIDItemInArr(data.key, userArr);
-        if(userArr[i] != data.val() && i != -1)
+        if (i != -1) {
           userArr.splice(i, 1);
+        }
       });
     };
 
@@ -219,7 +239,7 @@ window.onload = function instantiate() {
 
   function createFamilyElement(familyData){
     try{
-      testData.remove();
+      document.getElementById('testData').remove();
     } catch (err) {}
 
     let liItem = document.createElement("LI");
