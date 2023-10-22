e3bba35e5b3c9ab24d3be03c5149ae602f106743
Donovan Adrian <websurfer21020@gmail.com>
2023-10-21 22:55:54 -0600

    Updated secretSantaAlg File
    Updated secretSantaAlg JS file with reorganized variables/functions, updated list functions, and started progress on the family page functions.

--- src/js/secretSantaAlg.js
+++ src/js/secretSantaAlg.js
@@ -4,16 +4,31 @@
  * with written consent under any circumstance.
  */
 
-let hideSecretSantaName = true;
-
+//List Page Variables
 let secretSantaBtn;
 let secretSantaUserModal;
 let closeSecretSantaUserModal;
 let secretSantaNameText;
 let showSecretSantaAssignment;
 
-let mostRecentSecretSantaState = 0;
+//Family Page Variables
+let secretSantaSectionHeader;
+let secretSantaStateText;
+let secretSantaNextStateText;
+let secretSantaStatusText;
+let secretSantaStateBtn;
+let secretSantaShuffleBtn;
+let secretSantaAutoBtn;
+let secretSantaExportBtn;
 
+let secretSantaElements = [];
+let hideSecretSantaName = true;
+let mostRecentSecretSantaState = 0;
+let familyMemberSignUpMinimum = 3;
+
+/*
+* List Page Functions
+*/
 function initializeSecretSantaListPageVars() {
   secretSantaBtn = document.getElementById("secretSantaBtn");
   secretSantaUserModal = document.getElementById("secretSantaUserModal");
@@ -23,13 +38,17 @@ function initializeSecretSantaListPageVars() {
 
   userInitial = firebase.database().ref("users/" + user.uid);
   familyInitial = firebase.database().ref("family/");
+
+  secretSantaElements = [secretSantaBtn, secretSantaUserModal, closeSecretSantaUserModal, secretSantaNameText,
+    showSecretSantaAssignment];
+  verifyElementIntegrity(secretSantaElements);
 }
 
 function evaluateSecretSantaButton(familyData) {
   if (familyData.secretSantaState == undefined) {
-    familyData.secretSantaState = 0;
+    familyData.secretSantaState = 1;
     firebase.database().ref("family/" + familyData.uid).update({
-      secretSantaState: 0
+      secretSantaState: 1
     });
   }
   if (familyData.members == undefined)
@@ -168,4 +187,205 @@ function initializeSecretSantaDB() {
 
   listeningFirebaseRefs.push(userInitial);
   listeningFirebaseRefs.push(familyInitial);
+}
+
+/*
+* Family Page Functions
+*/
+function initializeSecretSantaFamilyPageVars() {
+  secretSantaSectionHeader = document.getElementById("secretSantaSectionHeader");
+  secretSantaStateText = document.getElementById("secretSantaStateText");
+  secretSantaNextStateText = document.getElementById("secretSantaNextStateText");
+  secretSantaStatusText = document.getElementById("secretSantaStatusText");
+  secretSantaStateBtn = document.getElementById("secretSantaStateBtn");
+  secretSantaShuffleBtn = document.getElementById("secretSantaShuffleBtn");
+  secretSantaAutoBtn = document.getElementById("secretSantaAutoBtn");
+  secretSantaExportBtn = document.getElementById("secretSantaExportBtn");
+
+  secretSantaElements = [secretSantaSectionHeader, secretSantaStateText, secretSantaNextStateText,
+    secretSantaStatusText, secretSantaStateBtn, secretSantaShuffleBtn, secretSantaAutoBtn, secretSantaExportBtn];
+  verifyElementIntegrity(secretSantaElements);
+}
+
+function initializeSecretSantaFamilyModalElements(familyData) {
+  console.log(familyData.secretSantaState);
+
+  if (familyData.secretSantaState == undefined) {
+    familyData.secretSantaState = 1;
+    firebase.database().ref("family/" + familyData.uid).update({
+      secretSantaState: 1
+    });
+  } else if (familyData.secretSantaState > 3 || familyData.secretSantaState < 1) {
+    familyData.secretSantaState = 1;
+    firebase.database().ref("family/" + familyData.uid).update({
+      secretSantaState: 1
+    });
+  }
+  if (familyData.members == undefined)
+    familyData.members = [];
+
+  if (familyData.secretSantaState == 1) {
+    secretSantaStateText.innerHTML = "Secret Santa State: Idle";
+    secretSantaNextStateText.innerHTML = "Next Secret Santa State: Ready";
+    secretSantaNextStateText.style.display = "block";
+    secretSantaStatusText.style.display = "none";
+    secretSantaShuffleBtn.style.display = "none";
+    secretSantaAutoBtn.style.display = "inline-block";
+    secretSantaExportBtn.style.display = "none";
+
+    secretSantaStateBtn.onclick = function() {
+      changeSecretSantaState(familyData, 2);
+    };
+    secretSantaAutoBtn.onclick = function() {
+      toggleAutomaticFunctionality();
+    };
+    secretSantaShuffleBtn.onclick = function() {};
+    secretSantaExportBtn.onclick = function() {};
+  } else if (familyData.secretSantaState == 2) {
+    secretSantaStateText.innerHTML = "Secret Santa State: Ready";
+    secretSantaNextStateText.innerHTML = "Next Secret Santa State: Active";
+    secretSantaStatusText.innerHTML = "Secret Santa Status: Ready To Assign Names";
+    secretSantaNextStateText.style.display = "block";
+    secretSantaStatusText.style.display = "block";
+    secretSantaShuffleBtn.style.display = "none";
+    secretSantaAutoBtn.style.display = "inline-block";
+    secretSantaExportBtn.style.display = "none";
+
+    secretSantaStateBtn.onclick = function() {
+      changeSecretSantaState(familyData, 3);
+    };
+    secretSantaAutoBtn.onclick = function() {
+      toggleAutomaticFunctionality();
+    };
+    secretSantaExportBtn.onclick = function() {
+      exportSecretSantaData(0, familyData.members);
+    };
+    secretSantaShuffleBtn.onclick = function() {};
+  } else if (familyData.secretSantaState == 3) {
+    secretSantaStateText.innerHTML = "Secret Santa State: Active";
+    secretSantaNextStateText.innerHTML = "Next Secret Santa State: Idle";
+    secretSantaStatusText.innerHTML = "Secret Santa Status: Nominal";
+    secretSantaNextStateText.style.display = "block";
+    secretSantaShuffleBtn.style.display = "inline-block";
+    secretSantaAutoBtn.style.display = "inline-block";
+    secretSantaExportBtn.style.display = "inline-block";
+
+    secretSantaStateBtn.onclick = function() {
+      changeSecretSantaState(familyData, 1);
+    };
+    secretSantaShuffleBtn.onclick = function() {
+      if (!assignUsersToNames(familyData.members)) {
+        deployNotificationModal(true, "Secret Santa Shuffle Failed!", "Assigning " +
+            "Secret Santa users was unsuccessful! Do these users have too many restrictions? Please try again..." +
+            "<br><br><br>NOTE: Restrictions are things like the amount of friends, family relationships, and more. " +
+            "If your user doesn't have any friends or is related to too many people, assignments will be unsuccessful!",
+            20);
+      }
+    };
+    secretSantaAutoBtn.onclick = function() {
+      toggleAutomaticFunctionality();
+    };
+    secretSantaExportBtn.onclick = function() {
+      exportSecretSantaData(1, familyData.members);
+    };
+  }
+}
+
+function changeSecretSantaState(familyData, desiredStateInt) {
+  let stateIndicatorText;
+  let changedStateConcatString;
+  let validStateChange = false;
+  let invalidStateChangeReason = "";
+  let invalidModalOpenTime = 10;
+
+  switch (desiredStateInt) {
+    case 1:
+      if (unassignAllFamilyMembers(familyData.members))
+        validStateChange = true;
+      invalidStateChangeReason = "Deactivating Secret Santa was unsuccessful! Please try again...";
+      stateIndicatorText = "Idle";
+      break;
+    case 2:
+      if (evaluateUserReadiness(familyData.members))
+        validStateChange = true;
+      if (familyData.members > familyMemberSignUpMinimum) {
+        invalidStateChangeReason = "Not enough members are signed up! Make sure that at least " +
+            familyMemberSignUpMinimum + " family members are signed up prior to trying again!";
+      } else {
+        invalidStateChangeReason = "There are not enough members in this family! Make sure that at least " +
+            familyMemberSignUpMinimum + " family members are in this family and signed up prior to trying again!";
+      }
+      stateIndicatorText = "Ready";
+      break;
+    case 3:
+      if (assignUsersToNames(familyData.members))
+        validStateChange = true;
+      invalidStateChangeReason = "Assigning Secret Santa users was unsuccessful! Do these users have too many " +
+          "restrictions? Please try again...<br><br><br>NOTE: Restrictions are things like the amount of friends, " +
+          "family relationships, and more. If your user doesn't have any friends or is related to too many people, " +
+          "assignments will be unsuccessful!";
+      stateIndicatorText = "Active";
+      break;
+  }
+
+  if (validStateChange) {
+    firebase.database().ref("family/" + familyData.uid).update({
+      secretSantaState: desiredStateInt
+    });
+
+    changedStateConcatString = familyData.name + "'s Secret Santa State has been successfully changed to " +
+        stateIndicatorText + "!";
+
+    familyData.secretSantaState = desiredStateInt;
+    deployNotificationModal(true, "State Changed!", changedStateConcatString, 4);
+    initializeSecretSantaFamilyModalElements(familyData);
+  } else {
+    if (invalidStateChangeReason.length > 150)
+      invalidModalOpenTime = 20;
+    deployNotificationModal(true, "Secret Santa State Change Failed!",
+        invalidStateChangeReason, invalidModalOpenTime);
+  }
+}
+
+function evaluateUserReadiness(familyMembers) {
+  let tempIndex = 0;
+  let tempSignUpCount = 0;
+  let familyReady = false;
+
+  for (let i = 0; i < familyMembers.length; i++) {
+    tempIndex = findUIDItemInArr(familyMembers[i], userArr, true);
+    if (tempIndex != -1) {
+      if (userArr[tempIndex].secretSanta == 1)
+        tempSignUpCount++;
+    }
+  }
+
+  if (tempSignUpCount >= familyMemberSignUpMinimum)
+    familyReady = true;
+
+  return familyReady;
+}
+
+function toggleAutomaticFunctionality() {//todo placeholder
+  alert("This button will eventually toggle secret santa automatic control");
+}
+
+function exportSecretSantaData(desiredExportType, familyMembers) {//todo placeholder
+  alert("This button will eventually export the secret santa data to a csv");
+  switch (desiredExportType) {
+    case 0:
+      //exporting all currently signed up users in family
+      break;
+    case 1:
+      //exporting all signed up users AND their assignments in family
+      break;
+  }
+}
+
+function unassignAllFamilyMembers(familyMembers) {//todo placeholder
+  return true;
+}
+
+function assignUsersToNames(familyMembers) {//todo placeholder
+  return true;
 }
