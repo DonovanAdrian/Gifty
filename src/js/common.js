e7dcd9e78d97a5864480b770cd759b5f29101ffc
Donovan Adrian <websurfer21020@gmail.com>
2023-11-11 11:45:50 -0700

    Updated common File
    Updated common file with added getMonthName function.

--- src/js/common.js
+++ src/js/common.js
@@ -1975,6 +1975,38 @@ function getLocalTime(UTCTime, shortHand) {
   return localTime;
 }
 
+function getMonthName(month) {
+  switch(month) {
+    case 0:
+      return "January";
+    case 1:
+      return "February";
+    case 2:
+      return "March";
+    case 3:
+      return "April";
+    case 4:
+      return "May";
+    case 5:
+      return "June";
+    case 6:
+      return "July";
+    case 7:
+      return "August";
+    case 8:
+      return "September";
+    case 9:
+      return "October";
+    case 10:
+      return "November";
+    case 11:
+      return "December";
+    default:
+      console.log("Invalid Month!");
+      break;
+  }
+}
+
 function cloneArray(inputArr) {
   let outputArr = [];
 
