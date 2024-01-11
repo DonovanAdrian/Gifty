65d1a64a064e03922aaf11740414bf8850fe87e9
Donovan Adrian <websurfer21020@gmail.com>
2024-01-10 23:26:00 -0700

    Updated confirmationAlg File
    Updated confirmationAlg JS file with consolidated error call.

--- src/js/confirmationAlg.js
+++ src/js/confirmationAlg.js
@@ -77,9 +77,7 @@ window.onload = function instantiate() {
 
       initializeBackBtn();
     } catch (err) {
-      console.log("Critical Error: " + err.toString());
-      updateMaintenanceLog(pageName, "Critical Initialization Error: " + err.toString() + " - Send This " +
-          "Error To A Gifty Developer.");
+      sendCriticalInitializationError(err);
     }
   }
 
