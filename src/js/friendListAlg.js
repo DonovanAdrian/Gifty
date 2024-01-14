209786d6a5b72b9f6851e798046ea2580d207da8
Donovan Adrian <websurfer21020@gmail.com>
2024-01-13 21:39:46 -0700

    Updated friendListAlg File
    Updated friendListAlg JS file with consolidated error call.

--- src/js/friendListAlg.js
+++ src/js/friendListAlg.js
@@ -203,9 +203,7 @@ window.onload = function instantiate() {
       initializeBackBtn();
       initializeSwapBtn();
     } catch (err) {
-      console.log("Critical Error: " + err.toString());
-      updateMaintenanceLog(pageName, "Critical Initialization Error: " + err.toString() + " - Send This " +
-          "Error To A Gifty Developer.");
+      sendCriticalInitializationError(err);
     }
   }
 
