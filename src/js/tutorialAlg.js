49181d45ceab27c86999ecca37afd0dc1bb848d2
Donovan Adrian <websurfer21020@gmail.com>
2024-02-14 20:35:49 -0700

    Added tutorialAlg File
    Added tutorialAlg JS file for new tutorial page.

---
+++ src/js/tutorialAlg.js
@@ -0,0 +1,155 @@
+/*
+ * This project was created by Donovan Adrian and any
+ * edits or changes must be confirmed as valid by Donovan
+ * with written consent under any circumstance.
+ */
+
+let tutorialElements = [];
+
+let moderationTableOfContentsDiv;
+let moderationTutorialContentDiv;
+let settingsNote;
+
+
+
+function getCurrentUser() {
+    getCurrentUserCommon();
+
+    if (user.moderatorInt == 1) {
+        moderationTableOfContentsDiv.style.display = "block";
+        moderationTutorialContentDiv.style.display = "block";
+    } else {
+        moderationTableOfContentsDiv.style.display = "none";
+        moderationTutorialContentDiv.style.display = "none";
+    }
+}
+
+window.onload = function instantiate() {
+    initializeTutorialPage();
+
+    function initializeTutorialPage() {
+        try {
+            failedNavNum = 18;
+            pageName = "Tutorial";
+            inviteNote = document.getElementById("inviteNote");
+            settingsNote = document.getElementById("settingsNote");
+            moderationTableOfContentsDiv = document.getElementById("moderationTableOfContentsDiv");
+            moderationTutorialContentDiv = document.getElementById("moderationTutorialContentDiv");
+
+            getCurrentUser();
+            commonInitialization();
+
+            tutorialElements = [offlineModal, offlineSpan, inviteNote, settingsNote, notificationModal, notificationTitle,
+                notificationInfo, noteSpan];
+
+            verifyElementIntegrity(tutorialElements);
+            alternateButtonLabel(settingsNote, "Settings", "Tutorial");
+
+            userInitial = firebase.database().ref("users/");
+
+            databaseQuery();
+
+            emailBtn.onclick = function () {
+                let supportStr = genSupport();
+                window.open("mailto:gifty.application@gmail.com?subject=Gifty Support #" + supportStr +
+                    "&body=Hey Gifty Support, %0D%0A%0D%0A%0D%0A%0D%0A Sincerely, " + user.userName);
+            };
+        } catch (err) {
+            sendCriticalInitializationError(err);
+        }
+    }
+
+    function databaseQuery() {
+        let fetchData = function (postRef) {
+            postRef.on("child_added", function (data) {
+                clearInterval(commonLoadingTimer);
+                clearInterval(offlineTimer);
+                let i = findUIDItemInArr(data.key, userArr, true);
+                if (i != -1) {
+                    localObjectChanges = findObjectChanges(userArr[i], data.val());
+                    if (localObjectChanges.length != 0) {
+                        userArr[i] = data.val();
+
+                        if (data.key == user.uid) {
+                            user = data.val();
+                        }
+                        saveCriticalCookies();
+                    }
+                } else {
+                    userArr.push(data.val());
+
+                    if (data.key == user.uid) {
+                        user = data.val();
+                    }
+                    saveCriticalCookies();
+                }
+            });
+
+            postRef.on("child_changed", function (data) {
+                let i = findUIDItemInArr(data.key, userArr, true);
+                if (i != -1) {
+                    localObjectChanges = findObjectChanges(userArr[i], data.val());
+                    if (localObjectChanges.length != 0) {
+                        if (consoleOutput)
+                            console.log("Updating " + userArr[i].userName + " to most updated version: " + data.val().userName);
+                        userArr[i] = data.val();
+
+                        if (data.key == user.uid) {
+                            user = data.val();
+                            updateFriendNav(user.friends);
+                            if (consoleOutput)
+                                console.log("Current User Updated");
+                        }
+                        saveCriticalCookies();
+                    }
+                }
+            });
+
+            postRef.on("child_removed", function (data) {
+                let i = findUIDItemInArr(data.key, userArr);
+                if (i != -1) {
+                    userArr.splice(i, 1);
+                    saveCriticalCookies();
+                }
+            });
+        };
+
+        fetchData(userInitial);
+
+        listeningFirebaseRefs.push(userInitial);
+    }
+};
+
+function genSupport() {
+    let supportCode = "";
+    for(let i = 0; i < 16; i++){
+        supportCode = supportCode + randomizer();
+    }
+    addSupportToDB(supportCode);
+    return supportCode;
+}
+
+function addSupportToDB(supportCode) {
+    let supportCount = 0;
+    try{
+        supportCount = supportArr.length;
+    } catch (err) {
+
+    }
+    if(consoleOutput) {
+        console.log(supportCode);
+        console.log(supportCount);
+    }
+    firebase.database().ref("users/" + user.uid + "/support/" + supportCount).push();
+    firebase.database().ref("users/" + user.uid + "/support/" + supportCount).set({
+        supportCount: supportCount,
+        supportString: supportCode
+    });
+}
+
+function randomizer() {
+    let alphabet = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
+    let selector = Math.floor((Math.random() * alphabet.length));
+    let charSelect = alphabet.charAt(selector);
+    return charSelect;
+}
